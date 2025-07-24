dce1ddebe0d3210a4386f102e3ce0e8b31c3653a
papuganivijaykumar <62068195+papuganivijaykumar@users.noreply.github.com>
2025-07-24 12:50:30 +0530

    Update github-api-integration-module.sh

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -6,7 +6,7 @@
 #added description
 #
 #practice git fecth and pull
-#
+# added git pull
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
